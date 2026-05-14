1b65005d9d3ddcc00ab12f8314127ef9cc90e2c8
Abraão Reis <andradeabraao23@gmail.com>
2026-05-13 21:04:41 -0300

    feat: repositorios de conexao com as entidades

---
+++ src/main/java/com/imobsys/system/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.imobsys.system.repository;
+
+import com.imobsys.system.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+}
